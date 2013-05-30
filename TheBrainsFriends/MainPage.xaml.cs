02c1a7e37a45ae0a68a69cc99dc13373e7c0216b
Ricardo Álvarez <ragdes1988@gmail.com>
2013-05-30 14:11:49 +0200

    Importación

---
+++ TheBrainsFriends/MainPage.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Animation;
+using System.Windows.Shapes;
+using Microsoft.Phone.Controls;
+
+namespace TheBrainsFriends
+{
+    public partial class MainPage : PhoneApplicationPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private void cmdPlay_Click(object sender, RoutedEventArgs e)
+        {
+            NavigationService.Navigate(new Uri("/Niveles.xaml", UriKind.Relative));
+        }
+    }
+}
---
+++ TheBrainsFriends/Niveles.xaml.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Animation;
+using System.Windows.Shapes;
+using Microsoft.Phone.Controls;
+
+namespace TheBrainsFriends
+{
+    public partial class Niveles : PhoneApplicationPage
+    {
+        public Niveles()
+        {
+            InitializeComponent();
+        }
+
+        private void cmdPrincipiante_Click(object sender, RoutedEventArgs e)
+        {
+            NavigationService.Navigate(new Uri("/Principiante.xaml", UriKind.Relative));
+        }
+
+        private void cmdIntermedio_Click(object sender, RoutedEventArgs e)
+        {
+            NavigationService.Navigate(new Uri("/Intermedio.xaml", UriKind.Relative));
+        }
+
+        private void cmdExperto_Click(object sender, RoutedEventArgs e)
+        {
+            NavigationService.Navigate(new Uri("/Experto.xaml", UriKind.Relative));
+        }
+    }
+}
